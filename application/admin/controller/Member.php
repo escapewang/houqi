49c0013267df04cd442398620595fc7b18010c7d
dadoyi <phperdoyi@163.com>
2017-11-25 14:40:06 +0800

    会员列表显示

---
+++ application/admin/controller/Member.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+ class Member extends Controller{
+        public function list(){
+            // var_dump(config('template.view_path'));
+            return view();
+        }
+    }
+ 
