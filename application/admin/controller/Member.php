342407f48e225f76f6be1746e32f6cf942733875
dadoyi <phperdoyi@163.com>
2017-11-25 15:24:27 +0800

    加载会员列表

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -2,7 +2,7 @@
 namespace app\admin\controller;
 use think\Controller;
  class Member extends Controller{
-        public function list(){
+        public function index(){
             // var_dump(config('template.view_path'));
             return view();
         }
