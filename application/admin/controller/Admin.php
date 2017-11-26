c8153bffe8a11d35a6a84e1349758d813c14e193
dadoyi <phperdoyi@163.com>
2017-11-27 02:11:13 +0800

    管理员删除添加操作 修改未完成

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -25,13 +25,72 @@ class Admin extends controller
         return view();
     }
 
-    public function add()
+    public function add(Request $request)
     {   
-
-      if(request()->isPost()){
-        echo 111;
+      if($request->isPost()){
+        // return 111;
+        $data=input('post.');
+        $query=Db::table('wd_admin_user');
+        $user=$query->where('username',$data['username'])->find();
+        if($user){
+            return json(array('msg'=>'管理员已存在','code'=>100));
+        }
+ 
+       $datas=[
+            'username'=>$data['username'],
+            'password'=>md5($data['password']),
+            'create_time'=>$data['create_time'],
+        ];
+        
+        $res=Db::name('admin_user')->insert($datas);   
+        // return $res;
+        if($res){
+            return json(array('msg'=>'添加成功','code'=>200));
+        }else{
+            return json(array('msg'=>'添加失败','code'=>0));
+        }
       }
         return view();
     }
-    
+
+   public function test(){
+        
+       if($request->isPost){
+           $data=input('get.');
+            
+        }
+   }
+
+   public function del(Request $request)
+   {
+        if($request->isPost()){
+            $data=input('get.');
+            $query=Db::table('wd_admin_user');
+            $res=$query->where('id',$data['id'])->delete();   //真删除
+            // $res=$query->update('status',0)->where($data['id']);  //伪删除
+            if($res){
+                return json(array('msg'=>'删除成功','code'=>200));
+            }else{
+                return json(array('msg'=>'删除失败','code'=>0));
+            }
+        }
+   }
+
+
+   public function edit(Request $request)
+   {  
+       $id=input('get.');
+
+        // if($request->isPost){
+         var_dump($id['id']) ;
+            
+        // }
+        return view();
+   }
+
+
+   public function check()
+   {
+
+   }
 }
